bf1ff6d1781cd258236547c99f3884f862710740
tajul islam <tajulislam199595@gmail.com>
2025-09-30 11:42:49 +0600

    total-commit

--- src/DashBoard/MyParcels.jsx
+++ src/DashBoard/MyParcels.jsx
@@ -1,9 +1,74 @@
+import { useQuery } from "@tanstack/react-query"
+import useAuth from "../pages/Hooks/useAuth"
+import useAxiousSecure from "../pages/Hooks/useAxiousSecure"
 
 
 const MyParcels = () => {
+    const {user}=useAuth()
+    const axiousSecure = useAxiousSecure()
+    const {data:parcels=[]} = useQuery({
+        queryKey:['parcels', user?.email],
+        queryFn:async ()=>{
+            const res = await axiousSecure.get(`/allparcels?email=${user.email}`)
+            return res.data
+        }
+    })
+    const alldata=parcels.parcels
   return (
-    <div>
-      <h2>parcels will come soon</h2>
+     <div className=" w-full">
+      <table className="table w-full">
+        <thead>
+          <tr className="bg-base-200">
+            <th>Sender</th>
+            <th>Parcel Type</th>
+            <th>Title</th>
+            <th>totalcost</th>
+            <th>Payment Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody 
+            className="w-full"
+          
+        >
+          {alldata.map((parcel) => (
+            <tr key={parcel._id} className="hover">
+              <td>{parcel.senderInfo.name}</td>
+              <td>{parcel.parcelInfo.type}</td>
+              <td>{parcel.parcelInfo.title}</td>
+              <td>tk{parcel.totalCost}</td>
+              <td>
+                {parcel.paymentStatus === "paid" ? (
+                  <span className="badge badge-success">Paid</span>
+                ) : (
+                  <span className="badge badge-error">Unpaid</span>
+                )}
+              </td>
+              <td className="space-x-2">
+                <button
+                  className="btn btn-sm btn-info"
+                  onClick={() => onView(parcel)}
+                >
+                  View
+                </button>
+                <button
+                  className="btn btn-sm btn-error"
+                  onClick={() => onDelete(parcel._id)}
+                >
+                  Delete
+                </button>
+                <button
+                  className="btn btn-sm btn-error"
+                  onClick={() => onDelete(parcel.totalCost)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/layout/DashBoard.jsx
+++ src/layout/DashBoard.jsx
@@ -1,9 +1,9 @@
-import {  Outlet } from "react-router-dom"
+import {  NavLink, Outlet } from "react-router-dom"
 
 
 const DashBoard = () => {
   return (
-    <div className="drawer lg:drawer-open">
+    <div className="drawer lg:drawer-open ">
   <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content ">
     {/* Page content here */}
@@ -36,7 +36,7 @@ const DashBoard = () => {
     <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
       {/* Sidebar content here */}
       
-      <li><a>Sidebar Item 1</a></li>
+      <li><NavLink to='/dashboard/myparcels'>myParcels</NavLink></li>
       <li><a>Sidebar Item 2</a></li>
     </ul>
   </div>
